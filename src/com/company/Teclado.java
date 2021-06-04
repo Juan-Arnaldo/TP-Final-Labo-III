d2cf1ffe302e4c58256b89d0f511b99909428a08
Cristian Urbieta <urbietacristian@gmail.com>
2021-06-04 19:00:46 -0300

    Update Teclado.java
    Corrección de constructor de Teclado.java y documentación de método.

--- src/com/company/Teclado.java
+++ src/com/company/Teclado.java
@@ -3,11 +3,16 @@ package com.company;
 import java.util.Scanner;
 
 public class Teclado {
-    private Scanner sc = new Scanner(System.in);
+    private Scanner sc;
 
     public Teclado() {
+        sc = new Scanner(System.in);
     }
 
+    /**
+     * Método para cargar por teclado el nombre de un artículo.
+     * @return nombre - nombre del artículo cargado.
+     */
     public String cargarNombreArticulo() {
         String nombre;
         System.out.println("Ingrese el nombre del artículo: ");
