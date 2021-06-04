39066985226878ae71467a00df8e2fa86c19c3d1
Cristian Urbieta <urbietacristian@gmail.com>
2021-06-04 19:46:09 -0300

    Método recargar cantidad
    Agregado método recargar por teclado en caso de validación fallida.

--- src/com/company/Teclado.java
+++ src/com/company/Teclado.java
@@ -9,14 +9,37 @@ public class Teclado {
         sc = new Scanner(System.in);
     }
 
+    // Métodos de Carga para Artículos
+
     /**
      * Método para cargar por teclado el nombre de un artículo.
      * @return nombre - nombre del artículo cargado.
      */
     public String cargarNombreArticulo() {
-        String nombre;
         System.out.println("Ingrese el nombre del artículo: ");
-        nombre = sc.nextLine();
-        return nombre;
+        return sc.nextLine();
     }
+
+    /**
+     * Método para cargar por teclado la cantidad deseada.
+     * @return cantidad deseada
+     */
+    public int cargarCantidadArticulo(){
+        System.out.println("Ingrese la cantidad del articulo: ");
+        return sc.nextInt();
+    }
+
+    /**
+     * Método para volver a cargar por teclado la cantidad deseada luego de la validación fallida.
+     * @return cantidad deseada
+     */
+    public int cargarNuevamenteCantidadArticulo(){
+        System.out.println("No se puede vender la cantidad ingresada.");
+        System.out.println("Ingrese nuevamente la cantidad del articulo: ");
+        return sc.nextInt();
+    }
+
+
+
+
 }
