b75697c8b05d8639200c7c37fcf87ae54585d41f
unknown <gomez.agustin1896@gmail.com>
2021-06-06 16:52:49 -0300

    comentarios y cambios en teclado

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,6 +13,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
         List<Articulo> listaArticulo = new ArrayList<Articulo>();
         List<Cliente> listaCliente = new ArrayList<Cliente>();
         List<Proveedor> listaProveedor = new ArrayList<Proveedor>();
@@ -21,77 +22,16 @@ public class Main {
 
     }
 
-    public static Cliente crearCliente() {
-        Scanner sc = new Scanner(System.in);
-        String nombre, direccion;
-        String telefono;
-        String cuit;
-        String email;
-        int codInterno = 0;
 
 
-
-        System.out.println("Ingrese el nombre del cliente: ");
-        nombre = sc.nextLine();
-
-        System.out.println("Ingrese la direccion del cliente: ");
-        direccion = sc.nextLine();
-
-        System.out.println("Ingrese el telefono del cliente: ");
-        telefono = sc.next();
-
-        System.out.println("Ingrese el cuit del cliente: ");
-        cuit = sc.next();
-
-        System.out.println("Ingrese el email del cliente: ");
-        email = sc.next();
-
-
-        Cliente cliente = new Cliente(nombre,direccion,cuit,telefono,email,codInterno);
-        return cliente;
-    }
-
-    public static Proveedor crearProv() {
-        Scanner sc = new Scanner(System.in);
-        String nombre, direccion;
-        String telefono;
-        String cuit;
-        String email;
-        int codInterno = 0;
-        String localidad;
-
-
-        System.out.println("Ingrese el nombre del cliente: ");
-        nombre = sc.nextLine();
-
-        System.out.println("Ingrese la direccion del cliente: ");
-        direccion = sc.nextLine();
-
-        System.out.println("Ingrese el telefono del cliente: ");
-        telefono = sc.next();
-
-        System.out.println("Ingrese el cuit del cliente: ");
-        cuit = sc.next();
-
-        System.out.println("Ingrese el email del cliente: ");
-        email = sc.next();
-
-        System.out.println("Ingrese la localidad del cliente: ");
-        localidad = sc.nextLine();
-
-
-        Proveedor proveedor = new Proveedor(nombre,direccion,cuit,telefono,email,codInterno,localidad);
-        return proveedor;
-    }
-
-    public static void loteDeClientes(ArrayList<Cliente> listaCliente){
+    /*public static void loteDeClientes(ArrayList<Cliente> listaCliente){
         Cliente clien1 = new Cliente("Cristian","Puerto","2000","30000","cristian@gmail.com",0);
         listaCliente.add(clien1);
         Cliente clien2 = new Cliente("Martin","En la calle","2222","11232","martin@gmail.com",1);
         listaCliente.add(clien2);
         Cliente clien3 = new Cliente("Agus","Pasaje","333","22222","chango@gmail.com",2);
         listaCliente.add(clien3);
-    }
+    }*/
 
 
 
--- src/com/company/Teclado.java
+++ src/com/company/Teclado.java
@@ -1,5 +1,8 @@
 package com.company;
 
+import com.company.Persona.Cliente;
+import com.company.Persona.Proveedor;
+
 import java.util.Scanner;
 
 public class Teclado {
@@ -181,4 +184,74 @@ public class Teclado {
         return sc.nextLine();
     }
 
+    /**
+     * Metodo para ingresar el nuevo cliente por teclado.
+     * @return Cliente creado.
+     */
+    public static Cliente crearCliente() {
+        Scanner sc = new Scanner(System.in);
+        String nombre, direccion;
+        String telefono;
+        String cuit;
+        String email;
+        int codInterno = 0;
+
+
+
+        System.out.println("Ingrese el nombre del cliente: ");
+        nombre = sc.nextLine();
+
+        System.out.println("Ingrese la direccion del cliente: ");
+        direccion = sc.nextLine();
+
+        System.out.println("Ingrese el telefono del cliente: ");
+        telefono = sc.next();
+
+        System.out.println("Ingrese el cuit del cliente: ");
+        cuit = sc.next();
+
+        System.out.println("Ingrese el email del cliente: ");
+        email = sc.next();
+
+
+        Cliente cliente = new Cliente(nombre,direccion,cuit,telefono,email,codInterno);
+        return cliente;
+    }
+
+    /**
+     * Metodo para ingresar el nuevo proveedor por teclado.
+     * @return Proveedor creado.
+     */
+    public static Proveedor crearProv() {
+        Scanner sc = new Scanner(System.in);
+        String nombre, direccion;
+        String telefono;
+        String cuit;
+        String email;
+        int codInterno = 0;
+        String localidad;
+
+
+        System.out.println("Ingrese el nombre del cliente: ");
+        nombre = sc.nextLine();
+
+        System.out.println("Ingrese la direccion del cliente: ");
+        direccion = sc.nextLine();
+
+        System.out.println("Ingrese el telefono del cliente: ");
+        telefono = sc.next();
+
+        System.out.println("Ingrese el cuit del cliente: ");
+        cuit = sc.next();
+
+        System.out.println("Ingrese el email del cliente: ");
+        email = sc.next();
+
+        System.out.println("Ingrese la localidad del cliente: ");
+        localidad = sc.nextLine();
+
+
+        Proveedor proveedor = new Proveedor(nombre,direccion,cuit,telefono,email,codInterno,localidad);
+        return proveedor;
+    }
 }
