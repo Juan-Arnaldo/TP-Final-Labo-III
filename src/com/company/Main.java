8a33627d6708b6b3842c34bc51104eab3d540b09
Cristian Urbieta <urbietacristian@gmail.com>
2021-06-16 23:07:31 -0300

    Corrección en los llamados a new Articulo()

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,8 +14,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Local Hardcodeado
-
 //        Local local = hardcodearLocal();
 
         Teclado teclado = new Teclado();
@@ -28,90 +26,6 @@ public class Main {
 
         menu.menuInicio(local);
 
-//        System.out.println("Bienvenido!");
-//        int op1=0;
-//
-//        do{
-//            op1 = menu.cargarOpcionMenuInicio();
-//
-//            switch (op1) {
-//                case 1:
-//                    local.nuevoCliente(menu.cargarNuevoCliente(local));
-//                    break;
-//                case 2:
-//                    local.mostrarListaClienteOptimizada();
-//                    break;
-//                case 3:
-//                    local.nuevoDescuentoTarjeta(menu.cargarNuevoDescuentoTarjeta(local));
-//                    local.mostrarDescuentos();
-//                    break;
-//                case 4:
-//                    caja.cargaVenta(menu.cargarNuevaVenta(local, local.getListaCajas().getElemento(0)));
-//                    break;
-//                case 5:
-//                    local.mostrarVentas();
-//                    break;
-//                case 6:
-//
-//                    break;
-//                case 7:
-//
-//                    break;
-//                case 8:
-//
-//                    break;
-//                case 9:
-//                    System.out.println("Saliendo..");
-//                    break;
-//                default:
-//                    System.out.println("Opcion erronea.\nVuelva a intentarlo.");
-//                    break;
-//            }
-//        }while (op1 != 9);
-
-//        System.out.println("Bienvenido!");
-//        int op1=0;
-//
-//        do{
-//            op1 = teclado.cargarOpMain();
-//
-//            switch (op1) {
-//                case 1:
-//                    local2.nuevoCliente(menu.cargarNuevoCliente(local2));
-//                    break;
-//                case 2:
-//                    local2.mostrarListaClienteOptimizada();
-//                    break;
-//                case 3:
-//                    local2.nuevoDescuentoTarjeta(menu.cargarNuevoDescuentoTarjeta(local2));
-//                    local2.mostrarDescuentos();
-//                    break;
-//                case 4:
-//                    local2.getListaCajas().getElemento(0).cargaVenta(menu.cargarNuevaVenta(local2, local2.getListaCajas().getElemento(0)), local2);
-//                    break;
-//                case 5:
-//                    local2.mostrarVentas();
-//                    break;
-//                case 6:
-//                    local2.nuevoArticulo(menu.cargarNuevoArticulo(local2));
-//                    break;
-//                case 7:
-//
-//                    break;
-//                case 8:
-//                    json.guardarLocalArchivo(local2);
-//                    break;
-//                case 9:
-//                    System.out.println("Saliendo..");
-//                    break;
-//                default:
-//                    System.out.println("Opcion erronea.\nVuelva a intentarlo.");
-//                    break;
-//            }
-//        }while (op1 != 9);
-
-
-
     }
 
     public static Local hardcodearLocal() {
@@ -121,9 +35,9 @@ public class Main {
         Cliente cliente2 = new Cliente("Juan Ignacio", "asd", "222", "321", "juan@gmail.com", "Zapp");
         Cliente cliente3 = new Cliente("Martin Arnaldo", "asd", "333", "321", "juan@gmail.com", "Juan");
 
-        Articulo art1 = new Articulo(1, "Tornillo", "asd", "dsa", 20, 50, 100, 10);
-        Articulo art2 = new Articulo(2, "Tornillo A", "asd", "dsa", 20, 50, 50, 10);
-        Articulo art3 = new Articulo(3, "Tornillo B", "asd", "dsa", 20, 10, 100, 10);
+        Articulo art1 = new Articulo("Tornillo", "asd", "dsa", 20, 50, 100);
+        Articulo art2 = new Articulo("Tornillo A", "asd", "dsa", 20, 50, 50);
+        Articulo art3 = new Articulo("Tornillo B", "asd", "dsa", 20, 10, 100);
 
         local.nuevoArticulo(art1);
         local.nuevoArticulo(art2);
